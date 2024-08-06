daba94df04f255b7803ed73b3f0ae9fd5edc3b80
miaaajl <liaojingyi112@gmail.com>
2024-08-06 21:33:07 +0100

    Delete cae/__init__.py

--- cae/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Description: This file is used to 
-import all the necessary modules and classes for the package."""
-from .model import Autoencoder, load_and_preprocess_data, reshape_and_compute_cov
-from .model import compute_covariances, kalman_filter, run_data_assimilation, binarize_image
-
-__all__ = ['Autoencoder', 'load_and_preprocess_data', 'reshape_and_compute_cov',
-           'compute_covariances', 'kalman_filter', 'run_data_assimilation',
-           'binarize_image']
